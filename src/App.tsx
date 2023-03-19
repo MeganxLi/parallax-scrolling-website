55fbd748a6785c0c2677ed662fc697535037d89b
Meiyi <ant19950601@gmail.com>
2023-03-19 21:48:17 +0800

    :memo: use gsap

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { styled } from '@linaria/react'
-import { Tween, Timeline } from 'react-gsap'
+import { gsap } from "gsap"
 
 import './App.css'
 
 const Card = styled.div<CardProps>`
-padding: 2em;
-color: ${(props) => props.color};
+  padding: 2em;
+  color: ${(props) => props.color};
 `
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  border-radius: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  background-color: blue;
+  font-weight: 600;
+  color: "#fff";
+`
+
 function App() {
   const [count, setCount] = useState(0)
+  const app = useRef(null)
+
+  useEffect(() => {
+    console.log("useEffect");
+
+    if (!app.current) return
+    gsap.to(app.current, { rotate: 360, duration: 5 })
+
+  }, [])
 
   return (
     <div className="App">
       <h1>Vite + React</h1>
-      <Timeline>
-        <Tween from={{ x: '0px' }} to={{ x: '100px' }} duration={1}>
-          <div>Slide to the right</div>
-        </Tween>
-      </Timeline>
+      <Box ref={app}>selector</Box>
       <Card color="blue">
         <button type="button" onClick={() => setCount((prev) => prev + 1)}>
           count is
