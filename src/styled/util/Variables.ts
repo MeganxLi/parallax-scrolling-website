0d6186818fbce42d9e902235196168df7f7c5810
Meiyi <ant19950601@gmail.com>
2023-03-21 23:33:33 +0800

    :lipstick: add variables colors

---
+++ src/styled/util/Variables.ts
@@ -0,0 +1,32 @@
+const colors = {
+  green: {
+    1: '#4E7B57',
+    2: '#69A575',
+    3: '#A9C1AE',
+  },
+  orange: {
+    1: '#CFA575',
+    2: '#DFC5A7',
+    3: '#F1EDE9',
+  },
+  yellow: {
+    1: '#D2C67A',
+    2: '#EFE5A4',
+    3: '#EFE5A4',
+  },
+  black: {
+    core: '#000000',
+    900: '#2d2d2d',
+    800: '#4b4b4b',
+    700: '#6f6f6f',
+    600: '#909090',
+    500: '#cccccc',
+    400: '#ececec',
+    300: '#f2f2f2',
+    200: '#f6f6f6',
+    100: '#f9f9f9',
+  },
+  white: '#FFFFFF',
+}
+
+export default colors
