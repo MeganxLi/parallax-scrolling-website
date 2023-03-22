f69bd738a398ac2b35a91bf8caceb5dc42968c61
Meiyi <ant19950601@gmail.com>
2023-03-22 23:55:46 +0800

    :memo: update App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,9 @@
-import { useEffect, useRef, useState } from 'react'
-
-import { styled } from '@linaria/react'
-import { gsap } from "gsap"
-
-
-const Card = styled.div<CardProps>`
-  padding: 2em;
-  color: ${(props) => props.color};
-`
-
-const Box = styled.div`
-  width: 100px;
-  height: 100px;
-  border-radius: 12px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  background-color: blue;
-  font-weight: 600;
-  color: "#fff";
-`
+import Loading from './pages/Loading'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const app = useRef(null)
-
-  useEffect(() => {
-    console.log("useEffect");
-
-    if (!app.current) return
-    gsap.to(app.current, { rotate: 360, duration: 5 })
-
-  }, [])
-
   return (
     <div className="App">
-      <Box ref={app}>selector</Box>
-      <Card color="blue">
-        <button type="button" onClick={() => setCount((prev) => prev + 1)}>
-          count is
-          {count}
-        </button>
-      </Card>
+      <Loading />
     </div>
   )
 }
