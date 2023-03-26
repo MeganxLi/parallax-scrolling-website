b4cd01bb628fd3c505c467a32d97fe52b1afdcff
Meiyi <ant19950601@gmail.com>
2023-03-26 19:09:27 +0800

    :wrench: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
-import { defineConfig } from 'vite'
+/* eslint-disable import/no-extraneous-dependencies */
+import linaria from '@linaria/rollup'
 import react from '@vitejs/plugin-react'
-import linaria from '@linaria/vite';
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    linaria({
-      include: ["**/*.{ts,tsx}"],
-      babelOptions: {
-        presets: ["@babel/preset-typescript", "@babel/preset-react"],
-      },
-    }),
-  ],
-  base: 'https://MeganxLi.github.io/parallax-scrolling-website/'
+  plugins: [react(), linaria()],
+  base: 'https://MeganxLi.github.io/parallax-scrolling-website/',
 })
