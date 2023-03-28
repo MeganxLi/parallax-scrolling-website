f0552183028eebe99333d07d336079518e9f39fd
Meiyi <ant19950601@gmail.com>
2023-03-28 22:38:24 +0800

    :wrench: add react svg vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import linaria from '@linaria/rollup'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), linaria({
-    include: ['**/*.{ts,tsx}'],
-    babelOptions: {
-      presets: ['@babel/preset-typescript', '@babel/preset-react'],
-    },
-  })],
+  plugins: [
+    react(),
+    linaria({
+      include: ['**/*.{ts,tsx}'],
+      babelOptions: {
+        presets: ['@babel/preset-typescript', '@babel/preset-react'],
+      },
+    }),
+    svgr(),
+  ],
   base: 'https://MeganxLi.github.io/parallax-scrolling-website/',
 })
