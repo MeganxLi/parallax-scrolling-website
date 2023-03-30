2c6928dc4df9352cd177d0adbb7d10252ca95330
Meiyi <ant19950601@gmail.com>
2023-03-30 23:12:27 +0800

    :memo: add Banner title

---
+++ src/pages/Home/Banner.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BannerBlock, BannerTittle } from '../../styled/pages/Home'
+
+const Banner = () => {
+  return (
+    <BannerBlock>
+      <BannerTittle>
+        <h5>4th</h5>
+        <h2 data-clone="THEF2E">THEF2E</h2>
+        <h3>互動式網頁設計</h3>
+        <p>前端 & UI 修煉精神時光屋</p>
+      </BannerTittle>
+    </BannerBlock>
+  )
+}
+
+export default Banner
