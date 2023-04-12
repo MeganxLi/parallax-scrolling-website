286449f491c927da01138e97ffd2586c0d358066
Meiyi <ant19950601@gmail.com>
2023-04-12 23:58:32 +0800

    :construction: add gsap in Question block

--- src/pages/Home/Question.tsx
+++ src/pages/Home/Question.tsx
@@ -1,9 +1,50 @@
 import CarouselList from "../../constants/QuestionCarousel"
 import { QuestionBlock, QuestionCarousel, QuestionCarouselIndicators, QuestionCarouselItem, QuestionMark } from "../../styled/pages/Home"
+import { useEffect, useRef } from 'react'
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const Question = () => {
+  const questionBlockRef = useRef<HTMLDivElement>(null)
+  const carouseItemRef = useRef<(HTMLDivElement | null)[]>([])
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    // 預設隱藏所有 carouseItem
+    gsap.set(carouseItemRef.current, { autoAlpha: 0, y: '100%' });
+
+    const srollTL = gsap.timeline({
+      scrollTrigger: {
+        trigger: questionBlockRef.current,
+
+        start: "top center", // 從questionBlockRef 的中央開始觸發
+        end: "+=100%", // 觸發點為畫面中央
+        toggleClass: "active",
+        scrub: true,
+        markers: true,
+      },
+    });
+
+    srollTL.to(carouseItemRef.current[0], {
+      autoAlpha: 1,
+      y: '20vh',
+      duration: 1, //動畫執行的時間長度
+    });
+    srollTL.to(carouseItemRef.current[1], {
+      autoAlpha: 1,
+      y: '0%',
+      duration: 1,
+    });
+    srollTL.to(carouseItemRef.current[2], {
+      autoAlpha: 1,
+      y: '-20vh',
+      duration: 1,
+    });
+  }, [])
+
   return (
-    <QuestionBlock>
+    <QuestionBlock ref={questionBlockRef}>
       <QuestionCarousel>
         <QuestionCarouselIndicators>
           {CarouselList.map((item: CarouselListType) => {
@@ -13,6 +54,7 @@ const Question = () => {
 
         {CarouselList.map((item: CarouselListType) => {
           return <QuestionCarouselItem
+            ref={(el) => (carouseItemRef.current = [...carouseItemRef.current, el])}
             key={item.text}
             color={item.color}
             boxColor={item.boxColor}
