0096c96ce8a4467d809869f6a587edcf60d3e10d
Meiyi <ant19950601@gmail.com>
2023-04-19 23:36:30 +0800

    :memo: update CircleWhite text in Start block

--- src/pages/Home/Start.tsx
+++ src/pages/Home/Start.tsx
@@ -1,7 +1,9 @@
 import { useRef } from 'react'
 
+import Tag from '../../components/Tag'
 import {
-  CircleTitle, CircleWhite, StartBlock, StartLoopText,
+  CircleWhiteMain,
+  CircleWhiteTitle, CircleWhite, StartBlock, StartLoopText, GreenSnow, CircleMainItem1,
 } from '../../styled/pages/Home'
 
 const Start = () => {
@@ -19,11 +21,22 @@ const Start = () => {
         ))}
       </StartLoopText>
       <CircleWhite>
-        <CircleTitle>
+        <CircleWhiteTitle>
           <img src="./images/StartTitleIllu.png" alt="start title background" />
           <h3>互動式網頁設計</h3>
-        </CircleTitle>
-        <img src="./images/GreenSnow.png" alt="green snow" />
+        </CircleWhiteTitle>
+        <CircleWhiteMain>
+          <CircleMainItem1>
+            <p>{'<hello-world />'}</p>
+            <Tag initText="UI設計師" fontSize={40} className="ui" />
+            <Tag initText="前端工程師" fontSize={40} className="front-end" />
+            <img src="./images/line-box.png" alt="line box" />
+          </CircleMainItem1>
+          <img src="./images/solution-three-circle.png" alt="solution three circle" />
+          <img src="./images/figma.png" alt="Figma logo" />
+          <Tag initText="Build Good Products Together" fontSize={48} className="build-good" />
+        </CircleWhiteMain>
+        <GreenSnow src="./images/GreenSnow.png" alt="green snow" />
       </CircleWhite>
 
       <StartLoopText ref={LoopTextRef}>
