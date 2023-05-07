9968415e2cd8cafe69a7f53a6e6e5cc968691b70
Meiyi <ant19950601@gmail.com>
2023-05-07 23:16:00 +0800

    :iphone: add Collaborate block RWD styled

--- src/styled/pages/Home/Collaborate.ts
+++ src/styled/pages/Home/Collaborate.ts
@@ -1,6 +1,6 @@
 import { styled } from '@linaria/react'
 
-// import { rwd } from '../../util/Mixins'
+import { rwd } from '../../util/Mixins'
 import colors from '../../util/Variables'
 
 export const CollaborateBlock = styled.div`
@@ -18,19 +18,54 @@ export const CollaborateTitle = styled.div`
   margin: 0 auto;
   width: fit-content;
   margin-bottom: 150px;
+
+  svg {
+    width: 50px;
+    height: 50px;
+  }
+
+  ${rwd('lg')} {
+    font-size: 24px;
+    margin-bottom: 3rem;
+
+    svg {
+    width: 20px;
+    height: 20px;
+   }
+  }
 `
 
 export const CollaborateContainer = styled.div`
   display: flex;
+  justify-content: center;
+  max-width: 1500px;
+  width: 100%;
+  margin: 0 auto;
+
+  ${rwd('md')} {
+    gap: 32px;
+    flex-direction: column;
+    align-items: flex-start;
+  }
 `
 
 export const CollaborateItem = styled.div`
-  width: 400px;
-  height: 400px;
+  width: 25%;
+  height: auto;
+  aspect-ratio: 1 / 1;
   border-radius: 50px;
   background-color: ${colors.white};
   display: flex;
   align-items: center;
   justify-content: center;
   margin: 0 auto;
+
+  img {
+    width: 80%;
+  }
+
+  ${rwd('lg')} {
+    padding: 18px;
+    border-radius: 10px;    
+  }
 `
