cc9c3e22a2a2fac19ba8a6d3096f7e018c14c85c
Meiyi <ant19950601@gmail.com>
2023-05-13 22:03:38 +0800

    :label: update type and styled typescript

--- src/types/state.d.ts
+++ src/types/state.d.ts
@@ -14,3 +14,18 @@ type CollaborateListType = {
   title: string,
   src: string
 }
+
+type ActivityContentListType = {
+  class_name: string,
+  title: string,
+  sub: string,
+  content: JSX.Element,
+}
+
+type ResolveListType = {
+  week: number,
+  title: string,
+  tag: string[],
+  ui_time: string,
+  font_time: string,
+}
--- src/types/styles.d.ts
+++ src/types/styles.d.ts
@@ -19,3 +19,7 @@ interface CarouselProps {
   boxColor: string
   order: number
 }
+
+interface ActivityItemProps {
+  variant: string
+}
