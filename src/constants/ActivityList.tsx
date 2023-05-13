57d52b6913cd79f885f9f285195932de1ec0d06e
Meiyi <ant19950601@gmail.com>
2023-05-13 22:04:27 +0800

    :building_construction: use jasn list manage

---
+++ src/constants/ActivityList.tsx
@@ -0,0 +1,48 @@
+const ActivityContentList: ActivityContentListType[] = [
+  {
+    class_name: 'activity-item-sign_up',
+    title: '報名時間',
+    sub: 'SING UP',
+    content: (
+      <>
+        開放報名
+        <br />
+        10 / 13(四) 早上 11: 00 - 11 / 6(日) 晚上 23: 59
+      </>
+    ),
+  },
+  {
+    class_name: 'activity-item-game_start',
+    title: '比賽開始',
+    sub: 'STAR',
+    content: (
+      <>
+        各組別開賽
+        <br />
+        10/31(一) UI組、團體組開賽
+        <br />
+        11/7 (一）前端組開賽
+      </>
+    ),
+  },
+  {
+    class_name: 'activity-item-work',
+    title: '登陸作品',
+    sub: 'UPLOAD',
+    content: (
+      <>
+        10/31(一) 中午 12:00 -
+        <br />
+        11/28(一) 中午 12:00
+      </>
+    ),
+  },
+  {
+    class_name: 'activity-item-live',
+    title: '線上直播',
+    sub: 'STREAM',
+    content: (<>11/3 - 11/24（每週四）</>),
+  },
+]
+
+export default ActivityContentList
--- src/pages/Home/Activity.tsx
+++ src/pages/Home/Activity.tsx
@@ -1,12 +1,15 @@
+import ActivityContentList from '../../constants/ActivityList'
 import {
-  ActivityBlock, ActivityContent, ActivityItemContent, ActivityItemSub, ActivityItemTitle,
-  GameStartText,
+  ActivityBlock,
+  ActivityContent,
+  ActivityItem,
+  ActivityItemContent,
+  ActivityItemSub,
+  ActivityItemTitle,
   GreenSmile,
   GreenSmileImg,
   GreenSmileText,
-  GreenSnow,
-  LiveText,
-  SignUpText, StarImgBottom, StarImgTop, WhiteLine, WorkText,
+  GreenSnow, StarImgBottom, StarImgTop, WhiteLine,
 } from '../../styled/pages/Home/Activity'
 
 const Activity = () => (
@@ -27,42 +30,13 @@ const Activity = () => (
     </GreenSmile>
     <ActivityContent>
       <StarImgTop src="./images/star.png" alt="Star" />
-      <SignUpText>
-        <ActivityItemTitle>報名時間</ActivityItemTitle>
-        <ActivityItemSub>SING UP</ActivityItemSub>
-        <ActivityItemContent>
-          開放報名
-          <br />
-          10/13(四) 早上 11:00 - 11/6(日) 晚上 23:59
-        </ActivityItemContent>
-      </SignUpText>
-      <GameStartText>
-        <ActivityItemTitle>比賽開始</ActivityItemTitle>
-        <ActivityItemSub>STAR</ActivityItemSub>
-        <ActivityItemContent>
-          各組別開賽
-          <br />
-          10/31(一) UI組、團體組開賽
-          <br />
-          11/7 (一）前端組開賽
-        </ActivityItemContent>
-      </GameStartText>
-      <WorkText>
-        <ActivityItemTitle>登陸作品</ActivityItemTitle>
-        <ActivityItemSub>UPLOAD</ActivityItemSub>
-        <ActivityItemContent>
-          10/31(一) 中午 12:00 -
-          <br />
-          11/28(一) 中午 12:00
-        </ActivityItemContent>
-      </WorkText>
-      <LiveText>
-        <ActivityItemTitle>線上直播</ActivityItemTitle>
-        <ActivityItemSub>STREAM</ActivityItemSub>
-        <ActivityItemContent>
-          11/3 - 11/24（每週四）
-        </ActivityItemContent>
-      </LiveText>
+      {ActivityContentList.map((item: ActivityContentListType) => (
+        <ActivityItem className={item.class_name} key={item.title}>
+          <ActivityItemTitle>{item.title}</ActivityItemTitle>
+          <ActivityItemSub>{item.sub}</ActivityItemSub>
+          <ActivityItemContent>{item.content}</ActivityItemContent>
+        </ActivityItem>
+      ))}
       <StarImgBottom src="./images/star.png" alt="Star" />
       <GreenSnow src="./images/green-snow.png" alt="green snow" />
     </ActivityContent>
