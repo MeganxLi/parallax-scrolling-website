297c52219849b37341033d4676364516dd815793
Meiyi <ant19950601@gmail.com>
2023-05-14 21:20:59 +0800

    :sparkles: add Resolve block

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,9 @@
+import Activity from './Activity'
 import Banner from './Banner'
 import Collaborate from './Collaborate'
 import Contest from './Contest'
 import Question from './Question'
+import Resolve from './Resolve'
 import Start from './Start'
 import Head from '../../components/Head'
 
@@ -11,6 +13,8 @@ const index = () => (
     <Banner />
     <Question />
     <Start />
+    <Activity />
+    <Resolve />
     <Contest />
     <Collaborate />
   </>
