cfb005128509905f66287d28508df83a70ea269a
Meiyi <ant19950601@gmail.com>
2023-06-11 22:39:05 +0800

    :construction: Resolve block gsap Work in progress

--- src/pages/Home/Resolve.tsx
+++ src/pages/Home/Resolve.tsx
@@ -1,3 +1,8 @@
+import { useEffect, useRef } from 'react'
+
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
 import { ReactComponent as RightArrowIcon } from '../../assets/svg/right_arrow.svg'
 import { ReactComponent as StarIcon } from '../../assets/svg/star.svg'
 import ResolveList from '../../constants/ResolveList'
@@ -16,45 +21,120 @@ import {
   ResolveTitleBlock,
 } from '../../styled/pages/Home/Resolve'
 
-const Resolve = () => (
-  <ResolveBlock>
-    <ResolveHeader>
-      <ResolveStar1><StarIcon /></ResolveStar1>
-      <ResolveStar2><StarIcon /></ResolveStar2>
-      <ResolveTitleBlock>
-        {ResolveList.map((item) => (
-          <ResolveItemTitle key={item.week} color={item.color} className={item.week === 1 ? 'active' : ''}>
-            {`Week${item.week}`}
-          </ResolveItemTitle>
-        ))}
-      </ResolveTitleBlock>
-    </ResolveHeader>
-    <ResolveBody>
-      {ResolveList.map((item) => (
-        <ResolveSection key={item.week} className={item.week === 1 ? 'active' : ''}>
-          <ResolveItemSub>
-            {`/ ${item.title} /`}
-          </ResolveItemSub>
-          {item.tag.map((tag) => (
-            <ResolveItemTag key={tag} color={item.color}>{`#${tag}`}</ResolveItemTag>
+const Resolve = () => {
+  const resolveBlockRef = useRef<HTMLDivElement>(null)
+  const resolveItemRef = useRef<(HTMLElement | null)[]>([])
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger)
+
+    // const scrollTL = gsap.timeline({
+    //   scrollTrigger: {
+    //     trigger: resolveBlockRef.current,
+    //     start: 'top top',
+    //     end: '+=1000',
+    //     pin: true, // 區塊固定
+    //     scrub: 1,
+    //     markers: true,
+    //   },
+    // })
+
+    const scrollTL = gsap.timeline({
+      scrollTrigger: {
+        trigger: resolveBlockRef.current,
+        start: 'top top',
+        end: 'bottom bottom',
+        pinSpacing: false,
+        pin: true,
+        markers: true,
+        scrub: 1,
+        onUpdate: (self) => {
+          const activeIndex = Math.floor(self.progress * 3)
+          resolveItemRef.current.forEach((item, index) => {
+            item?.classList.toggle('active', index === activeIndex)
+          })
+        },
+      },
+    })
+
+    scrollTL.to('.resolve-item', {
+      xPercent: -200,
+      duration: 2,
+      ease: 'power2.inOut',
+    })
+
+    // scrollTL
+    //   .to(resolveBlockRef.current, {
+    //     y: '0',
+    //   }, '<')
+    //   .to('.resolve-item', {
+    //     xPercent: -200,
+    //     onStart: () => {
+    //       resolveItemRef.current[0]?.classList.add('active')
+    //       resolveItemRef.current[1]?.classList.remove('active')
+    //       resolveItemRef.current[2]?.classList.remove('active')
+    //     },
+    //     onUpdate: () => {
+    //       resolveItemRef.current[0]?.classList.remove('active')
+    //       resolveItemRef.current[1]?.classList.add('active')
+    //       resolveItemRef.current[2]?.classList.remove('active')
+    //     },
+    //     onEnd: () => {
+    //       resolveItemRef.current[0]?.classList.remove('active')
+    //       resolveItemRef.current[1]?.classList.remove('active')
+    //       resolveItemRef.current[2]?.classList.add('active')
+    //     },
+    //   })
+  }, [])
+
+  const handleResolveItemRef = (el: HTMLElement | null) => {
+    resolveItemRef.current = [...resolveItemRef.current, el]
+  }
+  return (
+    <ResolveBlock ref={resolveBlockRef}>
+      <ResolveHeader>
+        <ResolveStar1><StarIcon /></ResolveStar1>
+        <ResolveStar2><StarIcon /></ResolveStar2>
+        <ResolveTitleBlock>
+          {ResolveList.map((item: ResolveListType) => (
+            <ResolveItemTitle
+              key={item.week}
+              ref={handleResolveItemRef}
+              color={item.color}
+              className={`resolve-item ${item.week === 1 ? 'active' : ''}`}
+            >
+              {`Week${item.week}`}
+            </ResolveItemTitle>
           ))}
-          <ResolveItemContent>
-            各組別投稿時間
-            <br />
-            UI 組投稿區間：
-            {item.ui_time}
-            <br />
-            前端組投稿區間：
-            {item.font_time}
-          </ResolveItemContent>
-          <ResolveItemButton>
-            更多關卡資訊
-            <RightArrowIcon />
-          </ResolveItemButton>
-        </ResolveSection>
-      ))}
-    </ResolveBody>
-  </ResolveBlock>
-)
+        </ResolveTitleBlock>
+      </ResolveHeader>
+      <ResolveBody>
+        {ResolveList.map((item) => (
+          <ResolveSection key={item.week} className={item.week === 1 ? 'active' : ''}>
+            <ResolveItemSub>
+              {`/ ${item.title} /`}
+            </ResolveItemSub>
+            {item.tag.map((tag) => (
+              <ResolveItemTag key={tag} color={item.color}>{`#${tag}`}</ResolveItemTag>
+            ))}
+            <ResolveItemContent>
+              各組別投稿時間
+              <br />
+              UI 組投稿區間：
+              {item.ui_time}
+              <br />
+              前端組投稿區間：
+              {item.font_time}
+            </ResolveItemContent>
+            <ResolveItemButton>
+              更多關卡資訊
+              <RightArrowIcon />
+            </ResolveItemButton>
+          </ResolveSection>
+        ))}
+      </ResolveBody>
+    </ResolveBlock>
+  )
+}
 
 export default Resolve
